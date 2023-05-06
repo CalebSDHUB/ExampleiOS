9a4ed507eb03331216b8afcbdb01e7788a038642
Caleb Danielsen <calebclientmanifold@gmail.com>
2023-05-06 18:24:45 +0200

    Complete feature-1

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var pressButton: UIButton!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var greatingLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func pressButtonTapped(_ sender: UIButton) {
+        greatingLabel.text = "Hello, \(nameTextField.text ?? "")"
+    }
+    
 
 }
 
